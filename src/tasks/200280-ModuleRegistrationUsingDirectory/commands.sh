6455318379f4ab479300ba7ebf14c9bf80b7be9e
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-12 17:06:02 +0530

    Added more info

--- src/tasks/200280-ModuleRegistrationUsingDirectory/commands.sh
+++ src/tasks/200280-ModuleRegistrationUsingDirectory/commands.sh
@@ -1,28 +1,6 @@
 cd ../../..
 
-cd src/tasks/200270-SimplePrismModule
+cd src/tasks/200280-ModuleRegistrationUsingDirectory
 
-cd src/apps/200270-SimplePrismModule
-
-dotnet new sln --name SimplePrismModule 
-
-dotnet new wpf --name SimplePrismShell --framework net7.0
-
-dotnet sln add ./SimplePrismShell/SimplePrismShell.csproj
-
-dotnet build SimplePrismModule.sln
-
-dotnet run --project ./SimplePrismShell/SimplePrismShell.csproj
-
-
-dotnet add ./SimplePrismShell/SimplePrismShell.csproj package Prism.DryIoc --version 8.1.97
-
-mkdir .\SimplePrismShell\Views
-
-# Just a marker file so that Visual Studio 2022 will recognize fthe folder.
-New-Item .\SimplePrismShell\Views\SomeMarkerFile.cs
-
-mkdir .\SimplePrismShell\ViewModels
-
-New-Item .\SimplePrismShell\ViewModels\SomeMarkerFile.cs
+cd src/apps/200280-ModuleRegistrationUsingDirectory
 
