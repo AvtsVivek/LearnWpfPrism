9d3b6d2ab4993900627eb6faa6ec5c7f6e5e1a59
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-13 10:24:18 +0530

    Added more info

--- src/apps/200340-ViewInjection/ModuleA/ModuleAModule.cs
+++ src/apps/200340-ViewInjection/ModuleA/ModuleAModule.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ModuleA
 {
@@ -22,7 +23,33 @@ namespace ModuleA
         public void OnInitialized(IContainerProvider containerProvider)
         {
             // The OnInitialized method will guarantee that the shell has been loaded and that the region has been created at this point
-            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+            IRegion region = _regionManager.Regions["ContentRegion"];
+
+            var view1 = containerProvider.Resolve<ViewA>();
+
+            var view2 = containerProvider.Resolve<ViewA>();
+            view2.Content = new TextBlock()
+            {
+                Text = "Hello from View 2",
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+
+            region.Add(view1);
+
+            region.Add(view2);
+
+            region.Activate(view2);
+
+            region.Activate(view1);
+
+            region.Deactivate(view1);
+
+            region.Activate(view2);
+
+            region.Remove(view2);
+
+            region.Activate(view1);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
