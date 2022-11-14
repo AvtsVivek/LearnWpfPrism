3cb7564210c6f299b93581272c49aa4d6d0e9e65
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-14 19:07:38 +0530

    New Project

---
+++ src/apps/200560-EventAggrigator/ModuleB/ModuleBModule.cs
@@ -0,0 +1,34 @@
+﻿
+using ModuleB.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ModuleB
+{
+    public class ModuleBModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleBModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _regionManager.RegisterViewWithRegion("MessageListRegion", typeof(MessageList));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ src/apps/200560-EventAggrigator/ModuleB/ViewModels/MessageListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using SimplePrismShell.Core.Events;
+using System.Collections.ObjectModel;
+
+namespace ModuleB.ViewModels
+{
+    public class MessageListViewModel : BindableBase
+    {
+        private ObservableCollection<string> _messages = new ObservableCollection<string>();
+        public ObservableCollection<string> Messages
+        {
+            get { return _messages; }
+            set { SetProperty(ref _messages, value); }
+        }
+
+        public MessageListViewModel(IEventAggregator eventAggregator)
+        {
+            eventAggregator.GetEvent<MessageSentEvent>().Subscribe(OnMessageReceived);
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            Messages.Add(message);
+        }
+    }
+}
