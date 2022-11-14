263bf643c20bf060d69691ea21337b2b3d29973c
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-14 09:56:54 +0530

    Renamed the folder

--- src/tasks/200400-ViewModelLocatorChangeConventions/commands.sh
+++
@@ -1,8 +0,0 @@
-cd ../../..
-
-cd src/tasks/200380-ViewModelLocator
-
-cd src/apps/200380-ViewModelLocator
-
-dotnet run --project ./SimplePrismShell/SimplePrismShell.csproj
-
---
+++ src/tasks/200400-VmLocatorChangeConventions/commands.sh
@@ -0,0 +1,8 @@
+cd ../../..
+
+cd src/tasks/200400-VmLocatorChangeConventions
+
+cd src/apps/200400-VmLocatorChangeConventions
+
+dotnet run --project ./SimplePrismShell/SimplePrismShell.csproj
+
