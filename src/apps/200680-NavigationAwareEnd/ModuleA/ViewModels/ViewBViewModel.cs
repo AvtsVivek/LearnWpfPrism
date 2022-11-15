dc5e95a3c1d1db7ac1da0a210b83d40f2b7909c2
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-15 12:28:03 +0530

    New Project

--- src/apps/200680-NavigationAwareEnd/ModuleA/ViewModels/ViewBViewModel.cs
+++ src/apps/200680-NavigationAwareEnd/ModuleA/ViewModels/ViewBViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ModuleA.ViewModels
 {
-    public class ViewBViewModel : BindableBase
+    public class ViewBViewModel : BindableBase, INavigationAware
     {
         private string _text = "ViewB";
         public string Text
@@ -26,5 +27,21 @@ namespace ModuleA.ViewModels
         {
 
         }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            PageViews++;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
     }
 }
--- src/apps/200680-NavigationAwareEnd/ModuleA/Views/ViewA.xaml.cs
+++ src/apps/200680-NavigationAwareEnd/ModuleA/Views/ViewA.xaml.cs
@@ -1,15 +1,33 @@
-﻿using System.Windows.Controls;
+﻿using Prism.Regions;
+using System.Windows.Controls;
 
 namespace ModuleA.Views
 {
     /// <summary>
     /// Interaction logic for ViewA.xaml
     /// </summary>
-    public partial class ViewA : UserControl
+    public partial class ViewA : UserControl, INavigationAware
     {
         public ViewA()
         {
             InitializeComponent();
+        }
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
+        int count = 0;
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+
+            _txtPageViews.Text = $"{++count}";
         }
     }
 }
